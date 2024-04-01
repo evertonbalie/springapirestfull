7f53a5430c46ba506f2c7d8b8a5d0048436c7238
Everton Rodrigues <ebr.1@hotmail.com>
2024-04-01 16:45:56 -0300

    alteraçãoes no pom.xml

---
+++ cursospringrestapi/src/main/java/curso/api/rest/model/Usuario.java
@@ -0,0 +1,48 @@
+package curso.api.rest.model;
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String login;
+	private String senha;
+	private String nome;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+
+}
